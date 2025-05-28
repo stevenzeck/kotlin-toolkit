882a85302a96c71ce2e3b7864f869ba34820cf2b
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2025-05-27 23:07:24 -0500

    Move Dokka 2.0 changes to separate branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,16 +27,32 @@ tasks.register("cleanDocs", Delete::class).configure {
     delete("${project.rootDir}/docs/readium", "${project.rootDir}/docs/index.md", "${project.rootDir}/site")
 }
 
-tasks.withType<DokkaTaskPartial>().configureEach {
-    dokkaSourceSets {
-        configureEach {
-            reportUndocumented.set(false)
-            skipEmptyPackages.set(false)
-            skipDeprecated.set(true)
-        }
-    }
+dependencies {
+    dokka(project(":readium:readium-streamer"))
+    dokka(project(":readium:readium-shared"))
+    dokka(project(":readium:readium-opds"))
+    dokka(project(":readium:adapters:exoplayer:readium-adapter-exoplayer-audio"))
+    dokka(project(":readium:navigators:media:readium-navigator-media-tts"))
+    dokka(project(":readium:navigators:media:readium-navigator-media-audio"))
+    dokka(project(":readium:navigators:media:readium-navigator-media-common"))
+    dokka(project(":readium:navigators:readium-navigator-common"))
+    dokka(project(":readium:readium-navigator"))
+    dokka(project(":readium:readium-lcp"))
+    dokka(project(":readium:adapters:pspdfkit:readium-adapter-pspdfkit-navigator"))
+    dokka(project(":readium:adapters:pspdfkit:readium-adapter-pspdfkit-document"))
+    dokka(project(":readium:adapters:pdfium:readium-adapter-pdfium-navigator"))
+    dokka(project(":readium:adapters:pdfium:readium-adapter-pdfium-document"))
+    dokka(project(":readium:adapters:pdfium:readium-adapter-pdfium-common"))
 }
 
-tasks.named<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>("dokkaGfmMultiModule").configure {
-    outputDirectory.set(file("${projectDir.path}/docs"))
+dokka {
+    dokkaSourceSets.configureEach {
+        reportUndocumented = false
+        skipEmptyPackages = false
+        skipDeprecated = true
+    }
+
+    dokkaPublications.html {
+        outputDirectory.set(layout.buildDirectory.dir("docs"))
+    }
 }
